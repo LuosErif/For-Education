9621672a152a3efe1c63c4b8669a7ab827d8771c
MichaelB <89157398+LuosErif@users.noreply.github.com>
2024-07-05 23:14:51 +0300

    Added First Basic tips
    Console.log(), variable declaration and a test for var declaration

---
+++ JavaScript/FrontEnd-JS/00-JS-Start/script.js
@@ -0,0 +1,7 @@
+//console.log('Hey world', 49328, 'Caupeda'); console.log('Hehe Caopeda maybe?') <- Можно и в одну строку но с явным разделением с semicolon
+console.log('Caupedaa ... ehhh') //CTRL + ? = line comment
+
+/*
+Многострочный комментарий
+*/
+
---
+++ JavaScript/FrontEnd-JS/01-JS-Next/script.js
@@ -0,0 +1,16 @@
+// Simple way to create a variable
+let variable; // semicolons are used to end a line of code manually
+variable = 12;
+// Faster way to create a variable
+let message = 'I am a message'; // <-- to put a string data use single '' symbols
+//var - a variable declarated by var can be used in code before the line it was declared on
+console.log(vared);
+var vared = Number(42);
+console.log(vared);
+
+function gonnaTry() {
+    console.log(vared);
+    var vared = Number(42);
+    console.log(vared);
+}
+gonnaTry();
